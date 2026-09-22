74ab5626576215fb230de5dae059a298b2c6fa05
SnehaBomma421 <2420030231@klh.edu.in>
2026-09-22 12:01:39 +0530

    Delete week2 directory

--- week2/drop_col_with_missing_vals.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np 
-df=pd.DataFrame({'Age':[25,30,np.nan,40,35], 'Department': ['HR', 'Finance','Finance', np.nan, 'IT']})
-print(df,"\n")
-df_drop_col=df.dropna(axis=1)
-print("DataSet after dropping columns with missing values:")
-print(df_drop_col)
