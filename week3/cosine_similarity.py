7cbfb89ca77407a8a412f19e16144be7799a94d8
SnehaBomma421 <2420030231@klh.edu.in>
2026-09-22 12:01:53 +0530

    Delete week3 directory

--- week3/cosine_similarity.py
+++
@@ -1,8 +0,0 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-s1="data science is fun"
-s2="science makes data useful"
-vectorizer=CountVectorizer().fit([s1,s2])
-vectors=vectorizer.transform([s1,s2])
-cos_sim=cosine_similarity(vectors[0],vectors[1])[0][0]
-print("Cosine Similarity:",cos_sim)
--- week3/spearman_dataset.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df=pd.read_csv("week3/Iris (3).csv")
-print(df.corr(method='spearman',numeric_only=float))
