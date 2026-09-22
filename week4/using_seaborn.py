d85de002c763d94cea5040d7189018a0f56503d0
SnehaBomma421 <2420030231@klh.edu.in>
2026-09-22 12:02:05 +0530

    Delete week4 directory

--- week4/using_seaborn.py
+++
@@ -1,9 +0,0 @@
-import seaborn as sns
-tip=sns.load_dataset("tips")
-print(tip.head())
-objective="classification: Survived (Yes/No)"
-success_criteria="Accuracy > 80%"
-constraints="Limited features, missing vals, imbalenced classes"
-print("Objective:",objective)
-print("Success: ",success_criteria)
-print("Constraints:",constraints)
